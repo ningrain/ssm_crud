8af9ab5e8086888d6a03feca886004b8ae04fd44
ningning.jiang <ningrain921@gmail.com>
2017-09-29 10:34:14 +0800

    commit

--- src/main/test/com/gta/BaseTest.java
+++ src/main/test/com/gta/BaseTest.java
@@ -12,5 +12,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
-public class BaseTest {
+public abstract class BaseTest {
 }
